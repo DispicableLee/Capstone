0c5a47123d198402317accbca02d511a8b249cce
DispicableLee <lee.robert053@gmail.com>
2022-01-31 12:32:01 +0800

    started on the dropdown functionality

--- dj/src/Components/AudioPlayer.js
+++ dj/src/Components/AudioPlayer.js
@@ -1,13 +1,13 @@
 import ReactAudioPlayer from "react-audio-player";
-import React from 'react';
+import React, { useEffect } from 'react';
 import {useState} from 'react';
 import axios from 'axios';
-import ReactPlayer from 'react-player'
 export default function AudioPlayer(props) {
 //===================================================================================================================
     //set the inital state of the name as null
     const [songName, setSongName] = useState("");
     const [response, setResponse] = useState("");
+    const [setPlay, setSetPlay] = useState(false);
     //this function is linked to the GET request,
     const handleRetrieve = async (event) =>{
         //prevent the page from reloading
@@ -16,7 +16,6 @@ export default function AudioPlayer(props) {
         const formData = new FormData();
         formData.append("songName", songName);
         console.log("formdata appended");
-        console.log(formData);
         try{
             const res = await axios({
                 method: "get",
@@ -26,22 +25,26 @@ export default function AudioPlayer(props) {
             console.log("RESPONSE");
             console.log(res.data)
             setResponse(res.data);
+            console.log(typeof res.data)
+            setSetPlay(true);
         }catch(error){
             console.log(error)
         }
     };
-    //response[data][1].data
     const handleNameSelect = (event) =>{
         setSongName(event.target.files[0].name);
     };
+    //Event Listener that sets {react-audio-player} autoplay to "true" after {handleRetrieve} is fired.
+    //useRef Function===================================================================================
 //=================================================================================================================================
     return (
       <div className="music-container">
         {/* REACT AUDIO PLAYER============================================= */}
         <ReactAudioPlayer
-        controls
-        autoPlay
-        src={response}/>
+        controls={true}
+        autoPlay={setPlay}
+        src={`http://localhost:8080/Uploadedfiles/${songName}`}/>
+        {/* Javascript Audio PLAYER===================================================== */}
       {/* FORM============================================== */}
           <form onSubmit={handleRetrieve} encType="multipart/form-data">
             <label>Choose a song to play</label>
--- dj/src/Components/SongList.js
+++ dj/src/Components/SongList.js
@@ -5,7 +5,6 @@ export default function SongList(props){
 //set the state of the slist as an emptey array
     const [slist, setSlist] = useState([])
    useEffect(async () =>{
-        
         try{
             const response = await axios({
                 method: "get",
@@ -20,22 +19,49 @@ export default function SongList(props){
         }
    },[])
           
+   
     
 //======================================================
     return(
         <div>
             <h1>Song List: </h1>
+            {/* create function "drop" */}
+
             {slist.map(
                 (name, index)=>{
                     return <div>
-                        <a key={index} href={"http://localhost:8080/Uploadedfiles" + '/' + `${name}`} >{name}</a>
-                        
+                        <a key={index} href={"http://localhost:8080/Uploadedfiles" + '/' + `${name}`} >{name}</a> 
                         </div>
                 }
             )}
             
-            
+            {/* create button that activates "drop" onClick() */}
+            <button>Show Downloaded songs</button>
         </div>
     )
-    
 }
+
+
+//=============================================================================================================================
+{/* <div class="dropdown">
+    <button onclick="show_list()" class="dropbtn">Images</button>
+    <div id="image_list" class="dropdown-content">
+        <input type="text" placeholder="Search for your images..." id="userInput">
+    </div>
+</div> */}
+
+
+
+// function show_list() {
+//     var image_list = document.getElementById("image_list");
+//     $.getJSON('http://localhost:8080/list-files', function(data) {
+//         data.forEach(function(file) {
+//             $(image_list).append($('<a href="http://localhost:8080/uploaded_files/' + file + '">' + file + '</a>'));
+//         })
+//     });
+// }
+//==============================================================================================================================
+
+
+
+//Need to create a div container for the dropdown
